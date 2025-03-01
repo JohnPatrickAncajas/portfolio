3936cdd19bbe17a8936e28025440a7e3bf2d79e6
JohnPatrickAncajas <143093214+JohnPatrickAncajas@users.noreply.github.com>
2025-03-01 22:46:38 +0800

    Version 0.22

--- components/Projects.js
+++ components/Projects.js
@@ -3,19 +3,19 @@
 import Image from "next/image";
 
 const projects = [
-  { id: 1, title: "Project One", description: "Short summary of project one.", image: "/images/project1.png" },
-  { id: 2, title: "Project Two", description: "Short summary of project two.", image: "/images/project2.png" },
-  { id: 3, title: "Project Three", description: "Short summary of project three.", image: "/images/project3.png" },
-  { id: 4, title: "Project Four", description: "Short summary of project four.", image: "/images/project4.png" },
-  { id: 5, title: "Project Five", description: "Short summary of project five.", image: "/images/project5.png" },
-  { id: 6, title: "Project Six", description: "Short summary of project six.", image: "/images/project6.png" }
+  { id: 1, title: "Dialecto", description: "Short summary of project one.", image: "/images/project1.png" },
+  { id: 2, title: "FlapItLawin", description: "Short summary of project two.", image: "/images/project2.png" },
+  { id: 3, title: "QuizMe", description: "Short summary of project three.", image: "/images/project3.png" },
+  { id: 4, title: "C Lessons", description: "Short summary of project four.", image: "/images/project4.png" },
+  { id: 5, title: "Memo", description: "Short summary of project five.", image: "/images/project5.png" },
+  { id: 6, title: "Academicart", description: "Short summary of project six.", image: "/images/project6.png" }
 ];
 
 export default function Projects() {
   return (
     <section id="projects" className="bg-gray-850 p-8 rounded-lg shadow-lg border border-gray-700 mt-12 backdrop-blur-md bg-opacity-30">
       <h2 className="text-xl font-semibold text-blue-400 mb-4">Projects</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-6">
         {projects.map((project) => (
           <div 
             key={project.id} 
