1c8fca3fbf1c6124a58a8187d80142f88452af12
JohnPatrickAncajas <143093214+JohnPatrickAncajas@users.noreply.github.com>
2025-03-02 10:23:57 +0800

    Version 0.26

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,8 @@ module.exports = {
           "0%": { opacity: "0", transform: "translateY(20px)" },
           "100%": { opacity: "1", transform: "translateY(0)" },
         },
-      }
-      
-      ,
+      },
       animation: {
-        train: "train s linear infinite",
         fadeIn: "fadeIn 0.6s ease-out",
       },
     },
